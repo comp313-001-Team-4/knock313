634438ef70d7171f38ac752d873b67861d69ca28
aravind449 <Aravindreddy449@gmail.com>
2021-02-02 22:02:27 -0500

    first commit

---
+++ knockknockapi/knockknockapi/src/test/java/com/comp313/knockknockapi/KnockknockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp313.knockknockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockknockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
